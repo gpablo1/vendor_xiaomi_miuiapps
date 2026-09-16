c6a869be8c31b5aac8872f4265e98f70c7daaf3d
gpablo1 <gpablo1@yahoo.com.ar>
2026-09-16 14:52:09 +0000

    miuiapps: update MiuiGallery to 4.3.1.15-global
    Update Xiaomi Gallery to 4.3.1.15-global for Android 17.
    
    Patch MIUI framework compatibility classes for AOSP and disable the Xiaomi photo-processing provider dependency to allow immediate viewing, editing and deletion.
    
    Add Gallery privileged/default permissions, sysconfig, MiCloud framework dependencies and shared-library declarations.
    
    Track MiuiGallery.apk with Git LFS.
    
    Change-Id: I3942c8959c2c2ba510117235ce4b75106b76e843

--- config.mk
+++ config.mk
@@ -3,7 +3,11 @@ PRODUCT_SOONG_NAMESPACES += \
 
 PRODUCT_PACKAGES += \
     MiuiGallery \
-    MiuiMediaViewer
+    MiuiMediaViewer \
+    privapp-com.miui.gallery.xml \
+    config-com.miui.gallery.xml \
+    default-com.miui.gallery.xml \
+    micloud-sdk.xml
 
 #PRODUCT_PACKAGES += \
 #    MiuiExtraPhoto \
@@ -13,3 +17,9 @@ PRODUCT_PACKAGES += \
 # permissions
 PRODUCT_COPY_FILES += \
     vendor/xiaomi/miuiapps/proprietary/system/etc/permissions/privapp-permissions-mediaviewer.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediaviewer.xml
+
+# Xiaomi Gallery framework dependencies
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/miuiapps/proprietary/system/framework/micloud-sdk.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/micloud-sdk.jar \
+    vendor/xiaomi/miuiapps/proprietary/system/framework/security-device-credential-sdk.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/security-device-credential-sdk.jar
+
